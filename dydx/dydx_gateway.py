935ad42088cc4bad2fa8b2b3c00ba57d6b5571b0
xldistance <819873074@qq.com>
2023-02-14 22:22:06 +0800

    Update dydx_gateway.py

--- dydx/dydx_gateway.py
+++ dydx/dydx_gateway.py
@@ -108,8 +108,6 @@ api_key_credentials_map: Dict[str, str] = {}
 class DydxGateway(BaseGateway):
     """
     * vn.py用于对接dYdX交易所的交易接口
-    * 进入https://trade.dydx.exchange/portfolio/overview，鼠标右键点击【检查，右上角>>找到【应用】，左侧【本地存储空间】/https://trade.dydx.exchange/找到【STARK_KEY_PAIRS】字典和【API_KEY_PAIRS】，需要用到STARK_KEY_PAIRS的privateKey(对应vn.py里面的stark_private_key)和API_KEY_PAIRS里面的key，secret，passphrase，其他参数不用改
-    * 链接到dydx的钱包资产必须走erc20链
     """
     default_setting: Dict[str, Any] = {
         "key": "",
